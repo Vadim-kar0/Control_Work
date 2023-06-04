9007dea206210a8dbfe1ab8a0830b0a71977ceef
вадим <funpalka1@gmail.com>
2023-06-04 13:20:32 +0400

    ввод массива с консоли

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new string[length];
+for (int i = 0; i < length; i++)
+{
+    Console.Write($"Введите {i+1} элемент массива: ");
+    array[i] = Console.ReadLine();
+}
