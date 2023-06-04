9c8d2d295bc3e31d4d8559d3069ea6d3a362c0bc
вадим <funpalka1@gmail.com>
2023-06-04 13:41:43 +0400

    Добавили метод печати массива в консоль

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 ﻿Console.Write("Введите длину массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
-int[] array = new string[length];
-for (int i = 0; i < length; i++)
+string[] array = new string[length];
+
+void FillArray(string[] array)
+{
+for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"Введите {i+1} элемент массива: ");
     array[i] = Console.ReadLine();
 }
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+FillArray(array);
+PrintArray(array);
+
